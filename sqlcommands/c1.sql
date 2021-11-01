4dd5858e70e8d83e99299d26cf2e05ca34696756
codyDon <donaldonserio20@gmail.com>
2021-11-01 20:23:48 +0300

    login commit

---
+++ sqlcommands/c1.sql
@@ -0,0 +1,3 @@
+-- SQLite
+INSERT INTO USERS (id, user_no, name, role, id_no, dob, gender, date_emp, end_date, remarks, password)
+VALUES (3, 'user003', '6LACK', 'ADMIN', 12345678, '2000-1-1', 'MALE', '2021-11-11', '2021-11-12', 'BAD', '12345');
